aefa30e802a16c2b1f13c166097c2de20f75c8fa
tfilteredc <132796135+tfilteredc@users.noreply.github.com>
2024-03-04 21:33:35 +0000

    Fix ObjectHandler

--- examples/TemplateServices/ObjectHandler.lua
+++ examples/TemplateServices/ObjectHandler.lua
@@ -3,36 +3,10 @@ local ReplicatedFirst = game:GetService("ReplicatedFirst")
 local CollectionService = game:GetService("CollectionService")
 local Catwork = require(ReplicatedFirst.Catwork)
 
--- TYPE DEFS
-export type Fragment = Catwork.Fragment<{
-	Instance: Instance,
-	ObjectConfig: {ExtraConfigs: Instance?, [string]: any},
-	[string]: any
-}>
-
-type FragmentParams = {
-	Instance: Instance,
-	ObjectConfig: {ExtraConfigs: Instance?, [string]: any},
-	
-	Name: string?,
-	Destroying: (Fragment) -> ()?,
-	Init: (Fragment) -> (),
-	[string]: any
-}
-
-export type Template = Catwork.Template<{}, FragmentParams>
-
-type TemplateParams = {
-	Name: string,
-	CreateFragment: (TemplateParams, params: FragmentParams) -> ()
-}
 
 -- BASE CODE
-local ObjectHandler: Catwork.TemplateService<
-	Fragment, Template,
-	FragmentParams, TemplateParams
->
-local instanceFragmentStore: {[Instance]: Fragment} = {}
+local ObjectHandler
+local instanceFragmentStore: {[Instance]: Catwork.Fragment<any>} = {}
 
 local function deriveObjectConfig(i)
 	local attr = i:GetAttributes()
@@ -44,45 +18,38 @@ local function registerObject(i: Instance, template)
 	local f = ObjectHandler:CreateFragmentFromTemplate(template, {
 		Instance = i,
 		ObjectConfig = deriveObjectConfig(i)
-	})	
-	instanceFragmentStore[i] = f
-
-	f:Spawn(function(ok, _, err)
-		if not ok then
-			warn(`{i}: {err}`)
-		end
-	end)
+	})
 end
 
 local function dropObject(i: Instance)
 	local f = instanceFragmentStore[i]
 	if not f then return end
 	instanceFragmentStore[i] = nil
-	
+
 	f:Destroy()
 end
 
 local function registerTemplate(template)
 	local name = template.Name
-	
+
 	for _, i in CollectionService:GetTagged(name) do
 		registerObject(i, template)
 	end
-	
+
 	CollectionService:GetInstanceAddedSignal(name):Connect(function(i)
 		registerObject(i, template)
 	end)
-	
+
 	CollectionService:GetInstanceRemovedSignal(name):Connect(dropObject)
 end
 
-ObjectHandler = Catwork.TemplateService {
+ObjectHandler = Catwork.Service {
 	Name = "ObjectHandler",
-	
-	TemplateAdded = function(self, template: TemplateParams)
+
+	TemplateAdded = function(self, template)
 		registerTemplate(template)
 	end,
-	
+
 	Fragment = function(self, params)
 		local i = params.Instance
 		if not i then
@@ -91,6 +58,17 @@ ObjectHandler = Catwork.TemplateService {
 
 		return Catwork:CreateFragmentForService(params, self)
 	end,
+
+	FragmentAdded = function(self, f)
+		local i = f.Instance
+		instanceFragmentStore[i] = f
+
+		f:Spawn(function(ok, _, err)
+			if not ok then
+				warn(`{i}: {err}`)
+			end
+		end)
+	end
 }
 
 return ObjectHandler
